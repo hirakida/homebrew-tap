2a979824a48eae3f2c13fb32d0afce977e3368d2
Yosuke Hirakida <12070156+hirakida@users.noreply.github.com>
2021-02-28 23:24:01 +0900

    Update ipapi.rb

--- Formula/ipapi.rb
+++ Formula/ipapi.rb
@@ -1,7 +1,7 @@
 class Ipapi < Formula
   desc "ip-api CLI"
-  homepage "https://github.com/hirakida/ip-api-cli"
-  url "https://github.com/hirakida/ip-api-cli/archive/v0.1.0.tar.gz"
+  homepage "https://github.com/hirakida/ipapi-cli"
+  url "https://github.com/hirakida/ipapi-cli/archive/v0.1.0.tar.gz"
   sha256 "9c33a04b4ac2bcaba16341de0750a2b98b2e7dc7b1eae0562386cbea489d191d"
   license "MIT"
 
